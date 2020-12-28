b4bfc48b5c3373d2d31ac2e5789f93ea7ace4b71
Erica Siegel <siegel.erica@gmail.com>
2020-12-27 16:58:12 -0800

    added code and tests failed then passed

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,36 @@
+
+const { test, expect } = require('@jest/globals');
+const Employee = require('../lib/Employee');
+
+test('creates employee object', () => {
+    const employee = new Employee('Erica', 10, 'siegel.erica@gmail.com');
+
+    expect(employee.name).toBe('Erica');
+    expect(employee.id).toBe(10);
+    expect(employee.email).toBe('siegel.erica@gmail.com');
+    
+});
+
+test("gets employee's name from prompt", () => {
+    const employee = new Employee('Erica');
+
+    expect(employee.getName()).toBe('Erica');
+});
+
+test("gets employee's id from prompt", () => {
+    const employee = new Employee(10);
+
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()));;
+});
+
+test("Can get email via getEmail()", () => {
+    const employee = new Employee('Erica', 10, 'siegel.erica@gmail.com');
+
+    expect(employee.getEmail()).toBe('siegel.erica@gmail.com');
+});
+
+test("getRole() will return 'Employee'", () => {
+    const employee = new Employee('Erica', 10, 'siegel.erica@gmail.com');
+    
+    expect(employee.getRole()).toBe('Employee');
+});
